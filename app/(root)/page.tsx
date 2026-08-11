00f3136448a9d75a3e4ffd56e0f2d1635c7b6e07
Basewrecker <siddhunrs@gmail.com>
2026-08-11 14:07:59 +0530

    limiter for products implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,13 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product-list";
+
 const Homepage = async () => {
-  return <>Prostore</>;
+  // console.log(sampleData)
+  return (
+    <>
+      <ProductList data={sampleData.products} title="Newset Arrivals" limit={4 } />
+    </>
+  );
 };
 
 export default Homepage;
